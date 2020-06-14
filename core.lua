a090d499f4067da69dcbf465c291555837f27724
zer420 <46166712+zer420@users.noreply.github.com>
2020-06-14 18:47:58 +0200

    Added DPI scaling for custom tab selector

--- core.lua
+++ core.lua
@@ -1,19 +1,20 @@
-local ui_enable = gui.Checkbox(gui.Reference("Misc", "General", "Extra"), "playerlist.enable", "Player List", false);
+local ui_enable = gui.Checkbox(gui.Reference("Misc", "General", "Extra"), "playerlist.enable", "Player List", true);
 local ui_win = gui.Window("playerlist", "Player List", 150, 150, 516, 422);
-
 
 local ui_tab = {
     {"Player", gui.Groupbox(ui_win, "Select a Player", 16, 56, 234), gui.Groupbox(ui_win, "Player Options", 266, 56, 234), gui.Groupbox(ui_win, "Player Information", 266, 224, 234),},
     {"Misc", gui.Groupbox(ui_win, "Playstyle", 16, 56, 234), gui.Groupbox(ui_win, "Priority Settings", 266, 56, 234),},
-    {1, {170,30,30,255}, {220,60,40,255}, {255,255,255,255}, {255,255,255,40}, f = draw.CreateFont("Calibri Bold", 20, 20),},
+    {1, {170,30,30,255}, {220,60,40,255}, {255,255,255,255}, {255,255,255,40}, f, dpi, dpi_scale = {0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3,},},
 };
 local ntab = (#ui_tab - 1); local function ui_setup() for i = 1, ntab do if i ~= ui_tab[ntab + 1][1] then for j = 2, #ui_tab[i] do ui_tab[i][j]:SetInvisible(true); end; end; end; end;
 ui_setup(); local function ui_tab_selector(x1, y1, x2, y2, active)
+    if ui_tab[ntab + 1].dpi ~= ui_tab[ntab + 1].dpi_scale[gui.GetValue("adv.dpi") + 1] then ui_tab[ntab + 1].dpi = ui_tab[ntab + 1].dpi_scale[gui.GetValue("adv.dpi") + 1];
+    ui_tab[ntab + 1].f = draw.CreateFont("Calibri Bold", 20 * ui_tab[ntab + 1].dpi); end;
     draw.SetFont(ui_tab[ntab + 1].f); local mx, my = input.GetMousePos(); local size = ((x2 - x1) / (ntab)); local offset = x1;
     for i = 1, (ntab) do
         if i == ui_tab[ntab + 1][1] then draw.Color(unpack(ui_tab[ntab + 1][3])) else draw.Color(unpack(ui_tab[ntab + 1][2])) end;
         draw.FilledRect(offset, y1, offset + size, y2); local xtxt, ytxt = draw.GetTextSize(ui_tab[i][1]);
-        draw.Color(unpack(ui_tab[ntab + 1][4])); draw.Text((offset + (size / 2) - (xtxt / 2)), (y1 + 20 - (ytxt / 2)), ui_tab[i][1]);
+        draw.Color(unpack(ui_tab[ntab + 1][4])); draw.Text((offset + (size / 2) - (xtxt / 2)), (y1 + ((y2 - y1) / 2) - (ytxt / 2)), ui_tab[i][1]);
         if mx >= offset and mx < offset + size and my >= y1 and my < y2 then
             draw.Color(unpack(ui_tab[ntab + 1][5])); draw.FilledRect(offset, y1, offset + size, y2);
             if input.IsButtonPressed("mouse1") then
@@ -21,7 +22,6 @@ ui_setup(); local function ui_tab_selector(x1, y1, x2, y2, active)
                 for j = 2, #ui_tab[i] do ui_tab[i][j]:SetInvisible(false); end;
 end; end; offset = offset + size; end; end;
 local ui_tab_select = gui.Custom(ui_win, "tab", 0, 0, 516, 40, ui_tab_selector); -- way cleaner than buttons
-
 
 local ui_plist = gui.Listbox(ui_tab[1][2], "players", 257);
 ui_plist:SetWidth(202);
@@ -293,5 +293,4 @@ callbacks.Register("CreateMove", "Apply", function()
             ResetPrio();
         end;
     end;
-
 end);
